4583eef2d6767dcc9cf2fcb32471e19ff56d3a07
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2018-06-07 18:25:01 +0530

    Main: basic command parser

--- cmd/Main.hs
+++ cmd/Main.hs
@@ -1,4 +1,48 @@
 module Main where
 
+import Protolude
+
+import qualified Options.Applicative as Opt
+import qualified Data.Text as Text
+
+import qualified MagicWormhole
+
+data Options
+  = Options
+  { cmd :: Command
+  , relayEndpoint :: MagicWormhole.WebSocketEndpoint
+  } deriving (Eq, Show)
+
+data Command
+  = Send
+  | Receive
+  deriving (Eq, Show)
+
+optionsParser :: Opt.Parser Options
+optionsParser
+  = Options
+    <$> commandParser
+    <*> Opt.option
+    (Opt.maybeReader MagicWormhole.parseWebSocketEndpoint)
+    ( Opt.long "relayserver-url" <>
+      Opt.help "Endpoint for the Relay server" <>
+      Opt.value defaultEndpoint <>
+      Opt.showDefault )
+  where
+    -- | Default URL for relay server.
+    --
+    -- This is a relay server run by Brian Warner.
+    defaultEndpoint = fromMaybe (panic "Invalid default URL") (MagicWormhole.parseWebSocketEndpoint "ws://relay.magic-wormhole.io:4000/v1")
+
+commandParser :: Opt.Parser Command
+commandParser = Opt.hsubparser
+  ( Opt.command "send" (Opt.info (pure Send) (Opt.progDesc "send a text message")) <>
+    Opt.command "receive" (Opt.info (pure Receive) (Opt.progDesc "receive a text message")) )
+
+opts :: Opt.ParserInfo Options
+opts = Opt.info (Opt.helper <*> optionsParser) (Opt.fullDesc <> Opt.header "wormhole")
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  options <- Opt.execParser opts
+  return ()
