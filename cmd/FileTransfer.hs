1c6755a15ac6b64b2c089fdc8aea4170170b321b
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2018-06-15 20:25:07 +0530

    file tranfer: send a bogus direct hint in the transit message and the file offer

--- cmd/FileTransfer.hs
+++ cmd/FileTransfer.hs
@@ -7,7 +7,7 @@ where
 
 import Protolude
 
-import qualified Data.Text as Text
+import qualified Data.Text.IO as TIO
 import qualified Crypto.Spake2 as Spake2
 import Data.Aeson
   ( FromJSON(..)
@@ -70,25 +70,53 @@ instance FromJSON ConnectionHint where
   -- TODO: 'asum' of Relay Hint parsing as well.
 
 instance ToJSON Transit where
-  toJSON (Transit as hs) = object [ "abilities-v1" .= toJSON as
-                                  , "hints-v1" .= toJSON hs ]
+  toJSON (Transit as hs) = object [ "transit" .= object [ "abilities-v1" .= toJSON as
+                                                        , "hints-v1" .= toJSON hs ] ]
   
 type Password = ByteString
 
 transitPurpose :: MagicWormhole.AppID -> ByteString
 transitPurpose (MagicWormhole.AppID appID) = toS appID <> "/transit-key"
 
--- sendFile :: MagicWormhole.Session -> Password -> FilePath -> IO () -- Response
--- sendFile session password filepath = do
+sendFile :: MagicWormhole.Session -> Password -> FilePath -> IO () -- Response
+sendFile session password filepath = do
 --   -- steps
 --   -- * first establish a wormhole session with the receiver and
 --   --   then talk the filetransfer protocol over it as follows.
---   -- * create abilities.
---   -- * derive a transit key
---   -- * send the transit message (dictionary with key as "transit" and value as abilities)
---   -- * answer??
---   -- * send offer message for the key "file"
---   -- * look for response dict with an "answer" key and value "{ 'file_ack': 'ok' }"
+  nameplate <- MagicWormhole.allocate session
+  mailbox <- MagicWormhole.claim session nameplate
+  peer <- MagicWormhole.open session mailbox  -- XXX: We should run `close` in the case of exceptions?
+  let (MagicWormhole.Nameplate n) = nameplate
+  printSendHelpText $ toS n <> "-" <> toS password
+  MagicWormhole.withEncryptedConnection peer (Spake2.makePassword (toS n <> "-" <> password))
+    (\conn -> do
+        -- create abilities
+        -- send transit message via wormhole as an ordinary message
+        let abilities' = [DirectTCP]
+        let hints' = [Direct "direct-hint-v1" 0.0 "127.0.0.1" 4848]
+        let transitMsg = Transit abilities' hints'
+        let encodedTransitMsg = toS (encode transitMsg)
+        -- send the transit message (dictionary with key as "transit" and value as abilities)
+        MagicWormhole.sendMessage conn (MagicWormhole.PlainText encodedTransitMsg)
+
+        -- receive the transit from the receiving side
+        MagicWormhole.PlainText answerMsg <- atomically $ MagicWormhole.receiveMessage conn
+        TIO.putStrLn (toS answerMsg)
+
+        -- send file offer message
+        let fileOffer = MagicWormhole.File (toS filepath) 10
+        MagicWormhole.sendMessage conn (MagicWormhole.PlainText (toS (encode fileOffer)))
+
+        -- receive file ack message {"answer": {"file_ack": "ok"}}
+        -- TODO: verify that file_ack is "ok"
+        MagicWormhole.PlainText rxFileOffer <- atomically $ MagicWormhole.receiveMessage conn
+        TIO.putStrLn (toS rxFileOffer)
+
+        -- we are now ready to prepare for the TCP communication
+        -- TODO derive a transit key
+        return ()
+    )
+
 --   -- * establish the tcp connection with the peer/relay
 --   -- * send handshake message:
 --   --     sender -> receiver: transit sender TXID_HEX ready\n\n
@@ -101,15 +129,6 @@ transitPurpose (MagicWormhole.AppID appID) = toS appID <> "/transit-key"
 --   -- * create record_keys (send_record_key and receive_record_key (secretboxes)
 --   -- * send the file (40 byte chunks) over a direct connection to either the relay or peer.
 --   -- * receiver, once it successfully received the file, sends "{ 'ack' : 'ok', 'sha256': HEXHEX }
---   nameplate <- MagicWormhole.allocate session
---   mailbox <- MagicWormhole.claim session nameplate
---   peer <- MagicWormhole.open session mailbox  -- XXX: We should run `close` in the case of exceptions?
---   let (MagicWormhole.Nameplate n) = nameplate
---   printSendHelpText $ toS n <> "-" <> toS password
---   MagicWormhole.withEncryptedConnection peer (Spake2.makePassword (toS n <> "-" <> password))
---     (\conn -> do
---         let offer = MagicWormhole.Message message
---         MagicWormhole.sendMessage conn (MagicWormhole.PlainText (toS (encode offer))))
   
   
 -- receiveFile :: Session -> Passcode -> IO Status
