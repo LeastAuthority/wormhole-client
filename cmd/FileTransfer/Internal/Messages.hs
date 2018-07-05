58a2b10c86642cb506b1d286c6a25c5385db005e
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2018-07-05 14:07:45 +0530

    add receive handshake test

--- cmd/FileTransfer/Internal/Messages.hs
+++ cmd/FileTransfer/Internal/Messages.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module FileTransfer.Internal.Messages
   ( makeSenderHandshake
+  , makeReceiverHandshake
   )
 where
 
@@ -11,7 +12,6 @@ import qualified Crypto.KDF.HKDF as HKDF
 import Crypto.Hash (SHA256(..))
 import qualified Crypto.Saltine.Internal.ByteSizes as ByteSizes
 import Data.Hex (hex)
-import Data.Text (toLower)
 
 makeSenderHandshake :: MagicWormhole.SessionKey -> ByteString
 makeSenderHandshake (MagicWormhole.SessionKey key) =
@@ -21,3 +21,12 @@ makeSenderHandshake (MagicWormhole.SessionKey key) =
         purpose = toS @Text @ByteString "transit_sender"
     in
       (toS @Text @ByteString "transit sender ") <> (hex hexid) <> (toS @Text @ByteString " ready\n\n")
+
+makeReceiverHandshake :: MagicWormhole.SessionKey -> ByteString
+makeReceiverHandshake (MagicWormhole.SessionKey key) =
+    let hexid = (HKDF.expand (HKDF.extract salt key :: HKDF.PRK SHA256) purpose keySize)
+        salt = "" :: ByteString
+        keySize = ByteSizes.secretBoxKey
+        purpose = toS @Text @ByteString "transit_receiver"
+    in
+      (toS @Text @ByteString "transit receiver ") <> (hex hexid) <> (toS @Text @ByteString " ready\n\n")
--- tests/MessagesTests.hs
+++ tests/MessagesTests.hs
@@ -14,4 +14,6 @@ tests = hspec $ do
   describe "sender handshake tests" $ do
     it "sender handshake for key = b\"123\"" $ do
       makeSenderHandshake (MagicWormhole.SessionKey (toS @Text @ByteString "123")) `shouldBe` (toS @Text @ByteString "transit sender 559BDEAE4B49FA6A23378D2B68F4C7E69378615D4AF049C371C6A26E82391089 ready\n\n")
+    it "receiver handshake for key = b\"123\"" $ do
+      makeReceiverHandshake (MagicWormhole.SessionKey (toS @Text @ByteString "123")) `shouldBe` (toS @Text @ByteString "transit receiver ED447528194BAC4C00D0C854B12A97CE51413D89AA74D6304475F516FDC23A1B ready\n\n")
 
