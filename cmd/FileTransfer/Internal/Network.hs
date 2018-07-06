57acadafb428c91ae58e6c1f0d0390e7189ab47e
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2018-07-06 09:50:20 +0530

    network: add a function to convert from word32 address to network address string

--- cmd/FileTransfer/Internal/Network.hs
+++ cmd/FileTransfer/Internal/Network.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module FileTransfer.Internal.Network
   ( allocateTcpPort
   , runTransitProtocol
@@ -27,7 +28,18 @@ import Network.Socket
   , setSocketOption
   , SocketOption( ReuseAddr )
   , AddrInfoFlag ( AI_NUMERICSERV )
+  , PortNumber( PortNum )
   )
+import Network
+  ( connectTo
+  , PortID(..)
+  )
+import Network.Info
+  ( getNetworkInterfaces
+  , NetworkInterface(..)
+  , IPv4(..)
+  )
+
 import qualified Control.Exception as E
 import qualified Crypto.Saltine.Class as Saltine
 import qualified Data.Text.IO as TIO
@@ -44,8 +56,47 @@ allocateTcpPort = E.bracket setup close socketPort
           _ <- bind sock (addrAddress addr)
           return sock
 
-runTransitProtocol :: SecretBox.Key -> [Ability] -> [ConnectionHint] -> IO ()
-runTransitProtocol key as hs = do
+type Hostname = Text
+
+ipv4ToHostName :: Word32 -> Hostname
+ipv4ToHostName ip =
+  let (q1, r1) = ip `divMod` 256
+      (q2, r2) = q1 `divMod` 256
+      (q3, r3) = q2 `divMod` 256
+  in
+    (show q3) <> "." <> (show r3) <> "." <> (show r2) <> "." <> (show r1)
+
+buildDirectHints :: IO [ConnectionHint]
+buildDirectHints = do
+  (PortNum portnum) <- allocateTcpPort
+  nwInterfaces <- getNetworkInterfaces
+  let nonLoopbackInterfaces =
+        filter (\nwInterface -> let (IPv4 addr4) = ipv4 nwInterface in addr4 /= 0x0100007f) nwInterfaces
+  return $ map (\nwInterface ->
+                  let (IPv4 addr4) = ipv4 nwInterface in
+                  Direct (Hint { hostname = ipv4ToHostName addr4
+                               , port = portnum
+                               , priority = 0
+                               , ctype = DirectTcpV1 })) nonLoopbackInterfaces
+
+
+data TCPEndpoint
+  = TCPEndpoint
+  { hint :: ConnectionHint
+  , ability :: Ability
+  , handle :: Handle
+  }
+
+tryToConnect :: Ability -> ConnectionHint -> IO (Maybe TCPEndpoint)
+tryToConnect a@(Ability DirectTcpV1) h@(Direct (Hint DirectTcpV1 _ hostname portnum)) = do
+  let port = PortNumber (fromIntegral portnum)
+  handle <- try (connectTo (toS hostname) port)
+  case handle of
+    Left (SomeException e) -> return Nothing
+    Right handle' -> return $ Just (TCPEndpoint h a handle')
+
+runTransitProtocol :: SecretBox.Key -> [Ability] -> [ConnectionHint] -> (TCPEndpoint -> IO a) -> IO ()
+runTransitProtocol key as hs app = do
   -- * establish the tcp connection with the peer/relay
   --  for each (hostname, port) pair in direct hints, try to establish
   --  a connection.
