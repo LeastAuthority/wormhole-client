4cfc03114c44c4617af1fd13db936c8f809c557f
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2018-07-19 18:02:34 +0530

    add a top level module for the library

---
+++ src/Transit.hs
@@ -0,0 +1,28 @@
+-- |
+-- Description : A Magic Wormhole client that supports file transfer.
+--
+-- Magic Wormhole is a technology for getting things from one computer to another, safely.
+--
+-- To use it, you must use the MagicWormhole library to first establish an encrypted connection:
+--
+--   1. Start a 'Rendezvous.Session' with the Rendezvous server, to allow peers to find each other ('Rendezvous.runClient')
+--   2. Negotiate a shared 'Messages.Nameplate' so peers can find each other on the server ('Rendezvous.allocate', 'Rendezvous.list')
+--   3. Use the shared 'Messages.Nameplate' to 'Rendezvous.open' a shared 'Messages.Mailbox'
+--   4. Use a secret password shared between peers to establish an encrypted connection ('Peer.withEncryptedConnection')
+--
+-- Once you've done this, you can communicate with your peer via 'Transit.send' and 'Transit.receive'.
+--
+-- The password is never sent over the wire.
+-- Rather, it is used to negotiate a session key using SPAKE2,
+-- and that key itself is used to derive many per-message keys,
+-- so that each message is encrypted using NaCl SecretBox.
+--
+module Transit
+  ( FileTransfer.send
+  , FileTransfer.receive
+  , FileTransfer.MessageType(..)
+  )
+where
+
+import qualified FileTransfer
+
