9e46978fc189927e1a5b45c14b415d9bb1e0d41c
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2018-08-16 21:28:23 +0530

    new functions for getting transit relay handshake and associated tests

--- tests/ProtocolTests.hs
+++ tests/ProtocolTests.hs
@@ -6,6 +6,7 @@ where
 import Protolude
 import Test.Hspec
 import qualified Crypto.Saltine.Class as Saltine
+import qualified Data.ByteString as BS
 
 import qualified MagicWormhole
 
@@ -21,7 +22,11 @@ tests = hspec $ do
       let rkey = (fromMaybe (panic "error decoding bytestring into secretbox key") $ Saltine.decode (toS @Text @ByteString "12345678901234567890123456789012"))
       makeReceiverHandshake rkey `shouldBe` (toS @Text @ByteString "transit receiver 3b0d65f31e63b490b4edc13cf27a8b09cfb53c479f8ab67bc984e9f392ea28f4 ready\n\n")
     it "sender relay handshake for a given key" $ do
+      sidea <- generateTransitSide
       let skey = (fromMaybe (panic "error decoding bytestring into secretbox key") $ Saltine.decode (toS @Text @ByteString "12345678901234567890123456789012"))
-          sidea = MagicWormhole.Side "0102030405060708"
-      makeSenderRelayHandshake skey sidea `shouldBe` (toS @Text @ByteString "please relay 24655fa61c1df5e320ee34d85417de170bcd5d31f69778600c2e3f78f2bd12b4 for side 0102030405060708\n")
-
+          MagicWormhole.Side s1 = sidea
+      makeRelayHandshake skey sidea `shouldBe` (toS @Text @ByteString "please relay 24655fa61c1df5e320ee34d85417de170bcd5d31f69778600c2e3f78f2bd12b4 for side " <> (toS @Text @ByteString s1) <> "\n")
+    it "sender relay handshake length" $ do
+      sidea <- generateTransitSide
+      let skey = (fromMaybe (panic "error decoding bytestring into secretbox key") $ Saltine.decode (toS @Text @ByteString "12345678901234567890123456789012"))
+      BS.length (makeRelayHandshake skey sidea) `shouldBe` (BS.length (toS @Text @ByteString "please relay  for side \n") + 32*2 + 8*2)
