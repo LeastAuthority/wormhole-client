12b88e2ea81b76648951a008e172f2cebda607e4
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2018-09-05 10:57:35 +0530

    more crypto round trip tests

--- tests/CryptoTests.hs
+++ tests/CryptoTests.hs
@@ -14,24 +14,24 @@ import qualified Hedgehog.Range as Range
 
 import Hedgehog (Property, Group(..), checkSequential)
 
-prop_cryptoRoundTripWithZeroNonce :: Property
-prop_cryptoRoundTripWithZeroNonce = property $ do
+prop_roundTripWithZeroNonce :: C.Purpose -> Property
+prop_roundTripWithZeroNonce purpose = property $ do
   secret <- forAll $ Gen.bytes (Range.singleton 32)
   let nonce = Saltine.zero
   let secret' = fromMaybe (panic "cannot decode secret") $ Saltine.decode secret
-  let key = C.deriveKeyFromPurpose C.SenderHandshake secret'
+  let key = C.deriveKeyFromPurpose purpose secret'
   let key' = fromMaybe (panic "cannot decode key") $ Saltine.decode key
   plaintext <- forAll $ Gen.bytes (Range.linear 1 256)
   let (Right (pt, _))  = C.encrypt key' nonce (C.PlainText plaintext) >>=
                         C.decrypt key'
   pt === C.PlainText plaintext
 
-prop_cryptoRoundTripWithNonZeroNonce :: Property
-prop_cryptoRoundTripWithNonZeroNonce = property $ do
+prop_roundTripWithNonZeroNonce :: C.Purpose -> Property
+prop_roundTripWithNonZeroNonce purpose = property $ do
   secret <- forAll $ Gen.bytes (Range.singleton 32)
   let nonce = Saltine.nudge Saltine.zero
   let secret' = fromMaybe (panic "cannot decode secret") $ Saltine.decode secret
-  let key = C.deriveKeyFromPurpose C.SenderHandshake secret'
+  let key = C.deriveKeyFromPurpose purpose secret'
   let key' = fromMaybe (panic "cannot decode key") $ Saltine.decode key
   plaintext <- forAll $ Gen.bytes (Range.linear 1 256)
   let (Right (pt, _))  = C.encrypt key' nonce (C.PlainText plaintext) >>=
@@ -41,6 +41,12 @@ prop_cryptoRoundTripWithNonZeroNonce = property $ do
 cryptoRoundTripTests :: IO Bool
 cryptoRoundTripTests =
   checkSequential $ Group "Crypto"
-  [ ("prop_cryptoRoundTripWithZeroNonce", prop_cryptoRoundTripWithZeroNonce)
-  , ("prop_cryptoRoundTripWithNonZeroNonce", prop_cryptoRoundTripWithNonZeroNonce)
+  [ ("sender handshake roundtrip with zero nonce", prop_roundTripWithZeroNonce C.SenderHandshake)
+  , ("sender handshake roundtrip with non-zero nonce", prop_roundTripWithNonZeroNonce C.SenderHandshake)
+  , ("receiver record roundtrip with zero nonce", prop_roundTripWithZeroNonce C.ReceiverRecord)
+  , ("receiver record roundtrip with non-zero nonce", prop_roundTripWithNonZeroNonce C.ReceiverRecord)
+  , ("sender record roundtrip with zero nonce", prop_roundTripWithZeroNonce C.SenderRecord)
+  , ("sender record roundtrip with non-zero nonce", prop_roundTripWithNonZeroNonce C.SenderRecord)
+  , ("relay handshake roundtrip with zero nonce", prop_roundTripWithZeroNonce C.RelayHandshake)
+  , ("sender record roundtrip with non-zero nonce", prop_roundTripWithNonZeroNonce C.RelayHandshake)
   ]
