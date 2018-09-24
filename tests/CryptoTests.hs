ba7b09dc1f6053ae2a7e1092ab2d95db2b2285ac
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2018-09-24 19:44:09 +0530

    CryptoTests: remove an unintended pattern match introduced for testing an assumption
    Also get rid of a few redundant `$'.

--- tests/CryptoTests.hs
+++ tests/CryptoTests.hs
@@ -5,28 +5,26 @@ module CryptoTests
 
 import Protolude
 
-import Hedgehog (forAll, property, (===), failure)
+import Hedgehog (forAll, property, (===), failure, Property, Group(..), checkSequential)
 
 import qualified Transit.Internal.Crypto as C
 import qualified Crypto.Saltine.Class as Saltine
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
-import Hedgehog (Property, Group(..), checkSequential)
-
 import qualified Generator
 
 prop_roundTrip :: Property
 prop_roundTrip = property $ do
-  purpose <- forAll $ Generator.purposeGen
+  purpose <- forAll Generator.purposeGen
   secret <- forAll $ Gen.bytes (Range.singleton 32)
-  nonceBytes <- forAll $ Generator.nonceBytesGen
+  nonceBytes <- forAll Generator.nonceBytesGen
   let nonce = fromMaybe (panic "cannot decode nonce") $ Saltine.decode nonceBytes
   let secret' = fromMaybe (panic "cannot decode secret") $ Saltine.decode secret
   let key = C.deriveKeyFromPurpose purpose secret'
   let key' = fromMaybe (panic "cannot decode key") $ Saltine.decode key
   plaintext <- forAll $ Gen.bytes (Range.linear 1 256)
-  let result@(Left e) = C.encrypt key' nonce (C.PlainText plaintext) >>= C.decrypt key'
+  let result = C.encrypt key' nonce (C.PlainText plaintext) >>= C.decrypt key'
   case result of
     Right (pt, _) -> pt === C.PlainText plaintext
     Left _ -> failure
